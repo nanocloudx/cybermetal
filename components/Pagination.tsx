fe8e39e4819307752f6a3695a53725bf099560df
なのくろ <nanocloudx@gmail.com>
2023-12-08 13:32:07 +0900

    update

--- components/Pagination.tsx
+++ components/Pagination.tsx
@@ -4,15 +4,31 @@ import {PropsWithChildren} from 'react'
 import {getPageCount} from '../pages/2023/pageCount'
 
 type Props = {
-  author: string
-  pageCount: number
+  author?: string
+  pageCount?: number
+  page?: number // deprecated
 }
 
 export const Pagination: React.FC<PropsWithChildren<Props>> = (props) => {
+  if (props.author && props.pageCount) {
+    return (
+      <div className={s.main}>
+        {props.children}
+        <p className={s.page}>{getPageCount(props.author, props.pageCount)}</p>
+      </div>
+    )
+  }
+  if (props.page) {
+    return (
+      <div className={s.main}>
+        {props.children}
+        <p className={s.page}>{props.page}</p>
+      </div>
+    )
+  }
   return (
     <div className={s.main}>
       {props.children}
-      <p className={s.page}>{getPageCount(props.author, props.pageCount)}</p>
     </div>
   )
 }
