df7258e09165c9bb59c41a3e39925cb32d321eda
なのくろ <nanocloudx@gmail.com>
2023-12-08 10:28:24 +0900

    update

--- pages/2023/kuri/8.tsx
+++ pages/2023/kuri/8.tsx
@@ -7,6 +7,8 @@ export default function Page() {
     <Pagination author={'kuri'} pageCount={8}>
       <h3>サイバーメタルチンピラガール</h3>
       <Image href={'/2023/kuri/kuri19.png'} widthPercentage={90}/>
+      <p>表紙の娘も生成してもらいました。呪文は「サイバーメタルチンピラガール」のみです。</p>
+      <p>ガールをつけないと男しか出てきませんでした。チンピラの多様化とAIのジェンダー配慮が急務です。</p>
     </Pagination>
   )
 }
